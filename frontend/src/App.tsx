7b79d30d25f53e72451aa0170b0453aa2134e2fd
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-01-16 19:48:52 +0530

    feat: button opens dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,9 +8,22 @@ import ReactFlow, {
   addEdge,
 } from "reactflow";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 // import '../app/globals.css';
 import "./index.css";
 import "reactflow/dist/style.css";
+import { Button } from "@/components/ui/button";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
@@ -45,5 +58,26 @@ const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 // }
 
 export default function App() {
-  return <h1 className="text-red-500 font-bold">Hello Hazelnode!</h1>;
+  return (
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button className="m-4" variant="outline">Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
