d7b615ef494fa30d04beeb8c6ec2919a3f0cd4dd
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-01-22 14:09:06 +0530

    chore: dialog by catalyst

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-// import React, { useCallback } from "react";
+import React from "react";
 // import ReactFlow, {
 //   MiniMap,
 //   Controls,
@@ -9,22 +9,19 @@
 // } from "reactflow";
 
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+  Dialog,
+  DialogActions,
+  DialogBody,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 // import '../app/globals.css';
 import "./index.css";
-import "reactflow/dist/style.css";
+// import "reactflow/dist/style.css";
 import { Button } from "@/components/ui/button";
-import { Badge } from "./components/ui/badge";
+import { Badge } from "@/components/ui/badge";
+import { Text } from "./components/ui/text";
 
 // const initialNodes = [
 //   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
@@ -59,30 +56,31 @@ import { Badge } from "./components/ui/badge";
 // }
 
 export default function App() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button className="m-4" color="lime">
-            Show Dialog
+      <Button color="lime" type="button" onClick={() => setIsOpen(true)}>
+        Show Dialog
+      </Button>
+      <Dialog open={isOpen} onClose={setIsOpen}>
+        <DialogTitle>Refund payment</DialogTitle>
+        <DialogDescription>
+          The refund will be reflected in the customer’s bank account 2 to 3
+          business days after processing.
+        </DialogDescription>
+        <DialogBody>
+          
+        </DialogBody>
+        <DialogActions>
+          <Button plain onClick={() => setIsOpen(false)}>
+            Cancel
           </Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+          <Button color="lime" onClick={() => setIsOpen(false)}>Refund</Button>
+        </DialogActions>
+      </Dialog>
 
-      <div className="flex gap-3">
+      <div className="mt-5 flex gap-3">
         <Badge color="lime">documentation</Badge>
         <Badge color="purple">help wanted</Badge>
         <Badge color="rose">bug</Badge>
