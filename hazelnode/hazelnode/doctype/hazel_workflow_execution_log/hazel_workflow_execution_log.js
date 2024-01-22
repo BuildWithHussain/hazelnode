0904a1d203a4e2bea703dceae37eedd32963bf62
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-01-22 19:41:09 +0530

    feat: more doctypes

---
+++ hazelnode/hazelnode/doctype/hazel_workflow_execution_log/hazel_workflow_execution_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Build With Hussain and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Hazel Workflow Execution Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
