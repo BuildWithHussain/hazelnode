a89aa1f8b2f147d6dad94f948bca6f296a3c9531
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-01-30 22:51:44 +0530

    fix: missing cardheader component

--- frontend/src/components/nodes/node.tsx
+++ frontend/src/components/nodes/node.tsx
@@ -1,16 +1,18 @@
 // import { useCallback } from 'react';
 import { Handle, NodeProps, Position } from 'reactflow';
-import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 
 export default function WorkflowNode({ data }: NodeProps<HazelNode>) {
   return (
     <>
       <Card>
-        <CardTitle className="flex items-center justify-between p-3">
-          <p>{data.type}</p>
-          <Button outline>Edit</Button>
-        </CardTitle>
+        <CardHeader>
+          <div className="flex items-center justify-between">
+            <CardTitle>{data.type}</CardTitle>
+            <Button outline>Edit</Button>
+          </div>
+        </CardHeader>
         <CardContent>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </CardContent>
